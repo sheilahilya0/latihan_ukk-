a839dbc045908687759a0dbf588b559061beda07
sheila_hilya <93960540+sheilahilya0@users.noreply.github.com>
2022-04-06 09:01:57 +0700

    Update Portofolio.php

--- latihan_ukk/admin/controllers/Portofolio.php
+++ latihan_ukk/admin/controllers/Portofolio.php
@@ -8,4 +8,21 @@ class Portofolio extends Controller
 
         $this->view('portofolio/index',$data); 
     }
+    public function pesan()
+    {
+       if ( $this->model('PortofolioModel')->tampungPesan($_POST) > 0) {
+            echo "
+            <script>
+            alert('Data berhasil dikirim')
+            window.location.href= 'http://localhost/latihan_ukk/public/'
+            </script>      
+            ";
+            
+        }
+        else{
+            echo "gagal terkirim
+            window.location.href= 'http://localhost/latihan_ukk/public/'
+            ";
+        }   
+    }
 }
